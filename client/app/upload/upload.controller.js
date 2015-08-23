f1c429402e20839a78194c36208e987c2163aeae
= <=>
2015-08-24 02:03:48 +0700

    finished modal window of advanced settings

--- client/app/upload/upload.controller.js
+++ client/app/upload/upload.controller.js
@@ -1,68 +1,66 @@
 'use strict';
 
 angular.module('cloudsfmApp')
-  .controller('UploadCtrl', function ($scope, $modal,  $window, Upload, $http) {
+  .controller('UploadCtrl', function($scope, $modal, $window, Upload, $http) {
 
     $scope.showThumb = true;
     $scope.uploaded = false;
 
     // for GET request
     $http.get('/api/uploadImages').success(function(index) {
-	$scope.index = index;
+      $scope.index = index;
+    });
+
+
+    // for advanced setting's modal
+    $scope.openAdvanced = function() {
+      $scope.modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        backdrop: 'static',
+        keyboard: false
       });
-     
+    };
 
-	// for advanced setting's modal
-	  $scope.openAdvanced = function () {
-		$scope.modalInstance = $modal.open({
-		  templateUrl: 'myModalContent.html',
-		  controller: 'ModalInstanceCtrl',
-		  backdrop: 'static',
-		  keyboard: false
-		});
-	  };
-	  
-	  
-	$scope.slider = {
-		'options': {
-			start: function (event, ui) { $log.info('Slider start'); },
-			stop: function (event, ui) { $log.info('Slider stop'); }
-		}
-	}
-			
 
-	// for upload router
+
+    // for upload router
     $scope.upload = function(files) {
-		$scope.numUploaded = 0; 
-        $scope.uploaded = true;
-        if (files && files.length) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                Upload.upload({
-                    url: 'api/uploadImages',
-                    method: 'POST',
-                    fields: {projectName: $scope.projectName,
-							 projectDescription: $scope.projectDescription
-							},
-                    file: file,
-                    fileFormDataName: 'cloudsfm'
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    	console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-					$scope.progress = progressPercentage;
-					// bind progress bar style
-					$scope.progressStyle = {width: $scope.progress + '%'};
-			
-				}).success(function (data, status, headers, config) {
-					console.log(headers);
-                    console.log('file ' + config.file.name + ' uploaded. Response: ' + data);
-                    $scope.numUploaded += 1;
-                    if ($scope.numUploaded === files.length) {
-						$window.location.href='/manage';
-					}
-                });
+      $scope.numUploaded = 0;
+      $scope.uploaded = true;
+      if (files && files.length) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          Upload.upload({
+            url: 'api/uploadImages',
+            method: 'POST',
+            fields: {
+              projectName: $scope.projectName,
+              projectDescription: $scope.projectDescription
+            },
+            file: file,
+            fileFormDataName: 'cloudsfm'
+          }).progress(function(evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config
+              .file.name);
+            $scope.progress = progressPercentage;
+            // bind progress bar style
+            $scope.progressStyle = {
+              width: $scope.progress + '%'
+            };
+
+          }).success(function(data, status, headers, config) {
+            console.log(headers);
+            console.log('file ' + config.file.name +
+              ' uploaded. Response: ' + data);
+            $scope.numUploaded += 1;
+            if ($scope.numUploaded === files.length) {
+              $window.location.href = '/manage';
             }
+          });
         }
+      }
     };
 
   });
